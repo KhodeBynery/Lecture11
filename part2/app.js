e134215ff86ef7a9e0733e3d3101d1b4fa17a7f2
anonmape <anonmape@greenwood.com>
2018-12-22 17:56:36 -0400

    Lecture11

--- part2/app.js
+++ part2/app.js
@@ -7,10 +7,17 @@ angular.module('MsgApp', [])
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.name = "Yaakov";
-
+  $scope.stateOfBeing = "hungry";
+  // $scope.stateOfBeing = "fed";
   $scope.sayMessage = function() {
     return "Yaakov likes to eat healthy snacks at night!";
-  }
+  };
+
+  $scope.feedYaakov = function functionName() {
+    $scope.stateOfBeing = "fed";
+  };
+
+
 }
 
 })();
